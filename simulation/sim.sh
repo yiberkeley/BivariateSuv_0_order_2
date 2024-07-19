5e5814b30ae093e679232372f1b56b0efaf3f782
yiberkeley <54548844+yiberkeley@users.noreply.github.com>
2024-07-19 16:26:32 -0700

    7

--- simulation/sim.sh
+++ simulation/sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=106
+#SBATCH --job-name=107
 #
 # Partition:
 #SBATCH --partition=savio3
@@ -24,4 +24,4 @@
 
 module load r
 
-R CMD BATCH --no-save sim.R sim106.Rout
+R CMD BATCH --no-save sim.R sim107.Rout
